ed5b07f709f47b807abdf5f6e3beb92ac0187e34
ujin-Bang <qkddnwls14@gmail.com>
2021-11-07 17:01:45 +0900

    xml 끌어와서 -> row 에 담아서 -> 내보내기  - 어댑터 뼈재작업 마무리

--- app/src/main/java/org/techtown/jickbangcopy/adapters/RoomAdapter.kt
+++ app/src/main/java/org/techtown/jickbangcopy/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import org.techtown.jickbangcopy.R
 import org.techtown.jickbangcopy.datas.RoomData
 
 class RoomAdapter (
@@ -15,6 +16,15 @@ class RoomAdapter (
     val mInflater = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
 
     }
 }
