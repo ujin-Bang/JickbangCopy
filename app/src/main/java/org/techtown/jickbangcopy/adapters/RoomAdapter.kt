e5e191a103fcb53808b50c1208382569085c84b6
ujin-Bang <qkddnwls14@gmail.com>
2021-11-07 16:53:36 +0900

    mInflater 생성, getView 오버라이딩

--- app/src/main/java/org/techtown/jickbangcopy/adapters/RoomAdapter.kt
+++ app/src/main/java/org/techtown/jickbangcopy/adapters/RoomAdapter.kt
@@ -1,11 +1,20 @@
 package org.techtown.jickbangcopy.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import org.techtown.jickbangcopy.datas.RoomData
 
 class RoomAdapter (
     val mContext: Context,
     val resId: Int,
-    val mList: ArrayList<RoomData>): ArrayAdapter<RoomAdapter>( mContext,resId,mList){
+    val mList: ArrayList<RoomData>): ArrayAdapter<RoomData>( mContext,resId,mList){
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+    }
 }
