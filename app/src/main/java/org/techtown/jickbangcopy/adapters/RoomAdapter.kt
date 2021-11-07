2ab327c3dd5ea5da98e3d066108bcf6f5b204664
ujin-Bang <qkddnwls14@gmail.com>
2021-11-07 16:40:58 +0900

    부모에게 넘겨주기 위한 파라미터들을 RoomAdapter의 생성자를 통해서 받아오게

--- app/src/main/java/org/techtown/jickbangcopy/adapters/RoomAdapter.kt
+++ app/src/main/java/org/techtown/jickbangcopy/adapters/RoomAdapter.kt
@@ -1,6 +1,11 @@
 package org.techtown.jickbangcopy.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
+import org.techtown.jickbangcopy.datas.RoomData
 
-class RoomAdapter : ArrayAdapter<RoomAdapter>(){
+class RoomAdapter (
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<RoomData>): ArrayAdapter<RoomAdapter>(){
 }
